1dca5230f350d4520b7acc3ecab06007ae64f7fa
kaushik072 <56080640+kaushik072@users.noreply.github.com>
2023-03-24 14:06:41 +0530

    remove positions

--- lib/views/localAndWebObjectsView.dart
+++ lib/views/localAndWebObjectsView.dart
@@ -88,6 +88,7 @@ class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
       customPlaneTexturePath: "assets/triangle.png",
       showWorldOrigin: true,
       handleTaps: false,
+      handleRotation: true
     );
     this.arObjectManager.onInitialize();
   }
@@ -100,9 +101,10 @@ class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
       var newNode = ARNode(
           type: NodeType.localGLTF2,
           uri: "assets/Chicken_01/Chicken_01.gltf",
-          scale: Vector3(0.2, 0.2, 0.2),
+          /*scale: Vector3(0.2, 0.2, 0.2),
           position: Vector3(0.0, 0.0, 0.0),
-          rotation: Vector4(1.0, 0.0, 0.0, 0.0));
+          rotation: Vector4(1.0, 0.0, 0.0, 0.0),*/
+      );
       bool? didAddLocalNode = await arObjectManager.addNode(newNode);
       localObjectNode = (didAddLocalNode!) ? newNode : null;
     }
@@ -117,7 +119,7 @@ class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
           type: NodeType.webGLB,
           uri:
           "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Fox/glTF-Binary/Fox.glb",
-          scale: Vector3(0.2, 0.2, 0.2));
+          /*scale: Vector3(0.2, 0.2, 0.2)*/);
       bool? didAddWebNode = await arObjectManager.addNode(newNode);
       webObjectNode = (didAddWebNode!) ? newNode : null;
     }
